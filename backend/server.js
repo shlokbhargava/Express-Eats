3f76cca330b7f98a6dc144609bde159a26dd72cf
shlokbhargava <shlokchamp88@gmail.com>
2021-08-15 18:13:05 +0530

    setup custom error handler

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 const db = require('./config/db')
 
 dotenv.config()
@@ -11,6 +12,10 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('API is running..........')
 })
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
